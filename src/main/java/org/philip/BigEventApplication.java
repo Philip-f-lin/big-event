bd97ac56e79ce08c60cde4c2a02429d917d6ffe4
linzhuofei <philip.f.lin@gmail.com>
2024-01-11 20:19:26 +0800

    Initial commit

---
+++ src/main/java/org/philip/BigEventApplication.java
@@ -0,0 +1,13 @@
+package org.philip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
