ecfddc5973b5edc090ce1dfb49eaecf9eeb59678
linzhuofei <philip.f.lin@gmail.com>
2024-01-26 20:42:00 +0800

    更新文章分類、添加文章分類，分組校驗

--- src/main/java/org/philip/pojo/Category.java
+++ src/main/java/org/philip/pojo/Category.java
@@ -3,6 +3,8 @@ package org.philip.pojo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.Default;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,7 +12,7 @@ import java.time.LocalDateTime;
 @Data
 public class Category {
 
-    @NotNull
+    @NotNull(groups = Update.class)
     private Integer id;//主键ID
     @NotEmpty
     private String categoryName;//分类名称
@@ -21,4 +23,17 @@ public class Category {
     private LocalDateTime createTime;//创建时间
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;//更新时间
+
+    // 如果說某個校驗項沒有指定分組，默認屬於 Default 分組
+    // 分組之間可以繼承，A extends B 那麼 A 中擁有 B 中所有的校驗項
+
+    public interface Add extends Default {
+
+    }
+
+    public interface Update extends Default{
+
+    }
+
+
 }
